f5cd76c4ff43c42d787d235a6ecba5f5e7d347dc
Hardeep Asrani <hardeepasrani@gmail.com>
2018-10-02 04:37:02 +0530

    Minor change
    Remove the else condition.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-if grep -Fxq "/* Add dynamic URLs */" /var/www/html/wp-config.php
+if ! grep -Fxq "/* Add dynamic URLs */" /var/www/html/wp-config.php
 then
-	ngrok http 80
-else
 	sed -i "/\/\* That's all, stop editing! Happy blogging. \*\//a\/\* Add dynamic URLs \*\/\ndefine('WP_SITEURL', 'http:\/\/' . \$_SERVER['HTTP_HOST']);\ndefine('WP_HOME', 'http:\/\/' . \$_SERVER['HTTP_HOST']);\n" wp-config.php
-	ngrok http 80
 fi
+ngrok http 80
